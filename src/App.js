9ed03c0fa19afbf06e0bc6bcb110da44759303c6
Luiz Henrique <luizhjramos@outlook.com>
2021-08-09 07:47:10 -0300

    Aula de useCallback

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 /* eslint-disable no-unused-vars */
+import P from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useCallback, useState } from 'react';
 
-const eventFn = () => {
-  console.log('h1 clicado');
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Eu também estou renderizando');
+  return <button onClick={() => incrementButton(10)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // useEffect(() => {
-  //   console.log('Executa sempre que o componenteß atualiza');
-  // });
-
-  // Executa apenas 1x
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn); // Removendo lixo do efeito anterior
-    };
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
   }, []);
 
-  useEffect(() => {
-    console.log('Executa toda vez que a dependência mudar');
-  }, [counter]);
+  console.log('Estou renderizando outra vez');
 
   return (
     <div className="App">
-      <p>aasass</p>
       <h1>Contador1: {counter}</h1>
-      <h1>Contador2: {counter2}</h1>
-      <button type="button" onClick={() => setCounter(counter + 1)}>
-        +
-      </button>
-      <button type="button" onClick={() => setCounter2(counter2 + 1)}>
-        + Counter2
-      </button>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
