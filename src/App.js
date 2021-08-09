e761e2763568a282b988c148e2ca8b45af6c015f
Luiz Henrique <luizhjramos@outlook.com>
2021-08-09 20:42:05 -0300

    Exemplo 01 - useMemo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import P from 'prop-types';
 import './App.css';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
+const Button = ({ incrementButton }) => {
   console.log('Eu também estou renderizando');
   return <button onClick={() => incrementButton(10)}>+</button>;
-});
+};
 
 Button.propTypes = {
   incrementButton: P.func,
@@ -21,10 +21,14 @@ function App() {
 
   console.log('Estou renderizando outra vez');
 
+  const btn = useMemo(() => {
+    return <Button incrementButton={incrementCounter} />;
+  }, [incrementCounter]);
+
   return (
     <div className="App">
       <h1>Contador1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      {btn}
     </div>
   );
 }
